8f96b295fcf561e8bdd1288501956079f045ce61
Eli Fine <ejfine@gmail.com>
2025-02-10 21:11:20 +0000

    pyinstaller

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -1,8 +1,8 @@
-import sys  # pragma: no cover # we can't unit test the entrypoint itself. It is tested in the E2E test of the executable itself
+import sys  # pragma: no cover # we can't unit test the entrypoint itself. It is tested in the E2E test of the executable
 
 from cloud_courier.main import (
     main,
-)  # pragma: no cover # we can't unit test the entrypoint itself. It is tested in the E2E test of the executable itself
+)  # pragma: no cover # we can't unit test the entrypoint itself. It is tested in the E2E test of the executable
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
