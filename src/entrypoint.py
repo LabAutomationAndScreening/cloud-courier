a2e83f53e4317a73e88b05d54261b4ed62d28968
Eli Fine <ejfine@gmail.com>
2025-02-10 20:56:59 +0000

    coiper

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -1,6 +1,8 @@
-import sys
+import sys  # pragma: no cover # we can't unit test the entrypoint itself. It is tested in the E2E test of the executable itself
 
-from cloud_courier.main import main
+from cloud_courier.main import (
+    main,
+)  # pragma: no cover # we can't unit test the entrypoint itself. It is tested in the E2E test of the executable itself
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
