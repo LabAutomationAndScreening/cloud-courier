3dd68304c534c2d45e66183adbed34b70a2904e1
Eli Fine <eli88fine@gmail.com>
2025-02-12 15:02:24 +0000

    add backslash

--- src/cloud_courier/aws_credentials.py
+++ src/cloud_courier/aws_credentials.py
@@ -24,7 +24,7 @@ def path_to_aws_credentials() -> Path:
     if (
         os.name == "nt"
     ):  # pragma: no cover # In Linux test environments, pathlib throws an error trying to run this: cannot instantiate 'WindowsPath' on your system
-        return Path("C:") / "Windows" / "System32" / "config" / "systemprofile" / ".aws" / "credentials"
+        return Path("C:\\") / "Windows" / "System32" / "config" / "systemprofile" / ".aws" / "credentials"
     return (  # pragma: no cover # In Windows test environments, pathlib will probably throw an error about this
         Path("/var") / "lib" / "amazon" / "ssm" / "credentials"
     )
